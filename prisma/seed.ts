14922116427d22de2c56e93faa140e647bb05e4a
Santiago Pruyas <santi.pruyas@gmail.com>
2026-04-07 09:13:46 -0300

    hotfix: admin password

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,5 @@
 import { PrismaClient, Role } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import bcrypt from "bcrypt";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -14,15 +13,13 @@ const prisma = new PrismaClient({ adapter });
 async function main() {
   console.log("🌱 Iniciando seed...");
 
-  const hashedPassword = await bcrypt.hash("admin123", 10);
-
   await prisma.user.upsert({
     where: { email: "admin@gobierno.gob.ar" },
     update: {},
     create: {
       email: "admin@gobierno.gob.ar",
       name: "Administrador Central",
-      password: hashedPassword,
+      password: "admin123", // Almacenar la contraseña sin hash para el seed, pero en producción se debe usar el hash
       role: Role.ADMIN,
     },
   });
